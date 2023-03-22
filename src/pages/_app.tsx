b9cb523a7c780f14d2e349e93e79ebe6126fbc83
Arun bala <arun.bala@yellow.ai>
2023-03-22 22:28:47 +0530

    build: added loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,32 @@
 import '@/styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import Home from '.';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
+import Loader from '../../components/Loader/Loader';
 import styles from '../styles/Home.module.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 1200);
+
   return (
     <ThemeProvider>
       <div className={styles.main}>
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            <Header />
+            <Home />
+            <Footer />
+          </>
+        )}
       </div>
     </ThemeProvider>
   );
